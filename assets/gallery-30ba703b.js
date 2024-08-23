1c1ccae4a282606b066dac004a66d0e040fa65a6
Nadiia-balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-08-23 21:19:08 +0000

    Deploying to gh-pages from @ Nadiia-balitska/minion-snake@2a8cd9b374fa51a34f152d52017accba7bbe82a6 🚀

---
+++ assets/gallery-30ba703b.js
@@ -0,0 +1,2 @@
+new Swiper(".horisontal-swiper-container",{direction:"horizontal",loop:!0,slidesPerView:4,navigation:{nextEl:".custom-next",prevEl:".custom-prev"},allowTouchMove:!0});
+//# sourceMappingURL=gallery-30ba703b.js.map
