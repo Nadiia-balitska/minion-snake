97ff031394f8ed39dce8e82a20b5fad9f9fc401f
seheichenko-anna <128909211+seheichenko-anna@users.noreply.github.com>
2024-08-24 09:22:52 +0000

    Deploying to gh-pages from @ Nadiia-balitska/minion-snake@8b3f675d265aae3cf2718924994ccafa82b4c5f5 🚀

--- assets/gallery-30ba703b.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".horisontal-swiper-container",{direction:"horizontal",loop:!0,slidesPerView:4,navigation:{nextEl:".custom-next",prevEl:".custom-prev"},allowTouchMove:!0});
-//# sourceMappingURL=gallery-30ba703b.js.map
---
+++ assets/main-b5215e5c.js
@@ -0,0 +1,2 @@
+import"./menu-3fb70584.js";new Swiper(".horisontal-swiper-container",{direction:"horizontal",loop:!0,slidesPerView:4,navigation:{nextEl:".custom-next",prevEl:".custom-prev"},allowTouchMove:!0});
+//# sourceMappingURL=main-b5215e5c.js.map
