f5bbe830827599692b3ae30ab8e3fd6ec0fefc0a
Nadiia-balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-08-24 19:03:40 +0000

    Deploying to gh-pages from @ Nadiia-balitska/minion-snake@9c7e484030a87dd878be96bafb819168748dbaad 🚀

--- assets/main-6a69a26f.js
+++
@@ -1,2 +0,0 @@
-import"./menu-00df3407.js";new Swiper(".horisontal-swiper-container",{direction:"horizontal",loop:!0,slidesPerView:4,navigation:{nextEl:".custom-next",prevEl:".custom-prev"},allowTouchMove:!0});
-//# sourceMappingURL=main-6a69a26f.js.map
---
+++ assets/main-ab6d4c75.js
@@ -0,0 +1,2 @@
+import"./menu-e02c8b48.js";new Swiper(".horisontal-swiper-container",{direction:"horizontal",loop:!0,slidesPerView:4,navigation:{nextEl:".custom-next",prevEl:".custom-prev"},allowTouchMove:!0});
+//# sourceMappingURL=main-ab6d4c75.js.map
